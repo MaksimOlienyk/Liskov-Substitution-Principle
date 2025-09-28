8431361e37e11628c506f85674d82c0585447d5e
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-09-28 18:25:19 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Liskov_substitution_principle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Enemy dragon = new Dragon();
+            dragon.Attack();
+
+            Enemy ogre = new Ogre();
+            ogre.Attack();
+
+            // FlyingEnemy ogre2 = new Ogre(); не прайює
+        }
+    }
+    interface Enemy
+    {
+        void Attack();
+    }
+
+    interface FlyingEnemy : Enemy
+    {
+        void Fly();
+    }
+
+    class Dragon : FlyingEnemy
+    {
+        public void Attack() => Console.WriteLine("Дракон б'є хвостом");
+        public void Fly() => Console.WriteLine("Дракон летить");
+    }
+
+    class Ogre : Enemy
+    {
+        public void Attack() => Console.WriteLine("Огр атакує");
+    }
+}
